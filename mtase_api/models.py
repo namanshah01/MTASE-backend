fecc5586affaa7f93144e2ce03f2e9edfb8710a2
Viraj Patidar <vrj381@gmail.com>
2022-03-17 10:38:42 +0530

    models

--- mtase_api/models.py
+++ mtase_api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class File(models.Model):
+    name  = models.CharField(max_length=100)
+    file = models.FileField(blank=False, null=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}-{self.timestamp}"
+
+
+class Text(models.Model):
+    text = models.TextField(blank=False)
+    # translatedText = models.TextField(blank=True)
+    # title = models.TextField(blank=False)
+    # summary = models.TextField(blank=False)
+    # textLength = models.IntegerField()
+    # summaryLength = models.IntegerField()
+    # original_language = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    
+    def __str__(self):
+        return f"{self.text}-{self.timestamp}"
