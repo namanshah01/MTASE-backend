25ad6bc9a6bc8fd06cd97f9252143f61cf80a7c5
Viraj Patidar <vrj381@gmail.com>
2022-03-17 10:38:29 +0530

    urls + settings

--- mtase/urls.py
+++ mtase/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mtase_api.urls')),
+    path('api/', include('mtase_api.urls')),
 ]
 
 
--- mtase_api/urls.py
+++ mtase_api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TestView
+from .views import TestView, SummariseAnalyseView
 
 urlpatterns = [
 
+    path('summarise', SummariseAnalyseView.as_view(), name="summarise"),
     path('test', TestView.as_view(), name="test"),
 
 ]
