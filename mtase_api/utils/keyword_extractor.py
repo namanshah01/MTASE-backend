870bdd90036c5f89f55aaf57400a00185effb02f
Viraj Patidar <vrj381@gmail.com>
2022-04-03 16:22:53 +0530

    minor fix

--- mtase_api/utils/keyword_extractor.py
+++ mtase_api/utils/keyword_extractor.py
@@ -5,6 +5,6 @@ def keyword_extractor(DOCUMENT):
     text = DOCUMENT
     rake_nltk_var.extract_keywords_from_text(text)
     keywords_extracted = rake_nltk_var.get_ranked_phrases()[:7]
-    print(keywords_extracted)
+    # print(keywords_extracted)
     
     return (keywords_extracted)
